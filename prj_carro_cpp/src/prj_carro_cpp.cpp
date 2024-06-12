75a01f99757db969d699e6ba8dd6441a789dd810
Bernardo de Resende Marcelino <bresende66@gmail.com>
2024-06-12 12:07:23 -0300

    Add files via upload

---
+++ prj_carro_cpp/src/prj_carro_cpp.cpp
@@ -0,0 +1,52 @@
+//============================================================================
+// Name        : prj_carro_cpp.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+// Defição da classe Carro
+class Carro{
+public:
+	//Membros da classe
+	std::string marca;
+	std::string modelo;
+	int ano;
+
+	// Construtor
+	Carro(const std::string &m, const std::string &mod, int a) :
+		marca(m), modelo(mod), ano(a){
+
+	}
+
+	// Construtor para criar um carro vazio
+	Carro() : marca(""), modelo (""), ano(0){
+	}
+
+	//Método para exibir informações sobre o carro
+	void exibirInformacoes(){
+		std::cout << "Marca:" << marca << ", Modelo: " << modelo << ", Ano: " << ano << std::endl;
+	}
+
+};
+
+
+int main (void) {
+	//Criando um objeto da classe Carro
+	Carro meuCarro;
+
+	//Criando um objeto da classe Carro usando o construtor
+	//Carro meuCarro ("Toyota", "Corolla", 20022);
+
+	//Atribuindo valores aos membros da classe
+	meuCarro.marca = "Toyota";
+	meuCarro.modelo = "Corolla";
+	meuCarro.ano = 2022;
+
+	//chamando o método para exibir informações sobre o carro
+	meuCarro.exibirInformacoes();
+	return 0;
+}
